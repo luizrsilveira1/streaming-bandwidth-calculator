50ae18a1bc719f9548f507038cb6321d8e10ab28
Luiz Ricardo Silveira <luizricsilveira@gmail.com>
2023-03-21 17:00:47 -0300

    Adicionando README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# streaming-bandwidth-calculator
+A library to calculate the bandwidth consumed through streaming connections.
--- converter/script.js
+++ converter/script.js
@@ -1,5 +1,3 @@
-// const {calculator} = require('./modules/calculator')
-// const {help} = require('./modules/help')
 import {calculator} from './modules/calculator.js'
 import {help} from './modules/help.js'
 
